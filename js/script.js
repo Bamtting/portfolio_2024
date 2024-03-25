c9594f6b165bd6e461d8945a02891422229c2b2b
Bamtting <ekekektmdals@gmail.com>
2024-03-25 19:43:47 +0900

    9

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
+    AOS.init();
     var swiper = new Swiper(".mySwiper", {
         navigation: {
           nextEl: ".swiper-button-next",
@@ -6,16 +7,6 @@ document.addEventListener("DOMContentLoaded", function(){
         },
       });
 
-       // gnb 링크를 클릭했을 때 스크롤 이벤트 처리
-    document.querySelectorAll('.gnb a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault(); // 기본 동작(링크 이동) 방지
-            const targetId = this.getAttribute('href').substring(1); // 링크의 href 속성값을 가져와 # 제거
-            const targetElement = document.getElementById(targetId); // 해당 섹션 요소를 가져옴
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' }); // 부드러운 스크롤 이동
-            }
-        });
-    });
+     
    });
    
