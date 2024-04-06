1bf43d1946cc72315001d28d7903fa9f5c4c9ad5
Bamtting <ekekektmdals@gmail.com>
2024-04-06 12:38:50 +0900

    new_info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
 var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+
+  var kracma = document.querySelector(".kracma");
+  var infoClose = document.querySelector(".portfolio_info_top .close");
+  var infoTwo = document.querySelector(".portfolio_info_box");
+ 
+kracma.addEventListener("click", function() {
+    infoTwo.classList.add("on");
+});
+infoClose.addEventListener("click", function() {
+    infoTwo.classList.remove("on");
+});
+
+
+  // 여기에 실행할 코드를 작성합니다.
+});
