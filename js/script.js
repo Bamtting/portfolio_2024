9f2dfab4c11edb8365e530de4a08b647bbb22de3
Bamtting <ekekektmdals@gmail.com>
2025-03-04 20:55:18 +0900

    project start and first commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-  var kracma = document.querySelector(".kracma");
-  var infoClose = document.querySelector(".portfolio_info_top .close");
-  var infoTwo = document.querySelector(".portfolio_info_box");
- 
-kracma.addEventListener("click", function() {
-    infoTwo.classList.add("on");
-});
-infoClose.addEventListener("click", function() {
-    infoTwo.classList.remove("on");
-});
-
 
 });
